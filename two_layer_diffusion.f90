a621c14d4bd4cfc0df9244e1b8d0dcc5fec49f3a
rniemeyer07 <rniemeyr@uw.edu>
2016-01-25 17:23:53 -0800

    2nd (updated) version of two-layer  diffusion model

--- two_layer_diffusion.f90
+++ two_layer_diffusion.f90
@@ -21,13 +21,15 @@ real, dimension(365*10) :: temp_change_ep, temp_change_hyp, energy
 real, dimension(365*10) :: energy_tot, diffusion_tot, T_in_tot, T_out_tot
 
 REAL, PARAMETER :: Pi = 3.1415927, prcnt_flow_epil = 0.2, prcnt_flow_hypo=0.8
-real, parameter :: v_t = 2.1 !diffusion coefficient (m2/day) - based on Snodgrass, 1974
+real, parameter :: v_t = 2.1 !diffusion coefficient (m/day)
+!    diffusion coefficient - based on Snodgrass, 1974
+
 real  :: flow_constant
 integer  :: i
 real :: x1, x2, x3
 
 real  :: depth_total, depth_e, depth_h, width, length, volume_e_x, outflow_x
-real :: energy_x, volume_h_x, area, density, heat_c, temp_change
+real :: energy_x, volume_h_x, area, density, heat_c, temp_change, delta_t
 real  :: flow_in_hyp_x, flow_in_epi_x, flow_out_epi_x, flow_out_hyp_x
 real  :: epix, hypox, dif_epi_x, dif_hyp_x, x, flow_epi_x, flow_hyp_x
 
@@ -36,9 +38,10 @@ real  :: epix, hypox, dif_epi_x, dif_hyp_x, x, flow_epi_x, flow_hyp_x
 open(unit=10, file="temp_change_epilim.dat")
 
 depth_total = 30
-width = 200
-length = 17000
+width = 400
+length = 50000
 area = width*length
+delta_t = 1 ! time is days,  assumes all units in equations are in days
 
 !-------------------------------------------------------------------------
 !     generate flow and energey temporal variables
@@ -62,10 +65,11 @@ do  i=0,10*365
    ! ------ get flow in to the reservoir for the year ------
    flow_in(i) =  sin( i/flow_constant)
    flow_in(i) = (flow_in(i) + 2)*30000   ! gets flow to vary from 30000 to 90000 cfs
-
+   flow_in(i) = (flow_in(i)/35.315)*60*60*24  ! converts ft3/sec to m3/day
    ! ------ get flow in to the reservoir for the year ------
    flow_out(i) =  sin( i/flow_constant)
    flow_out(i) = (flow_out(i) + 2)*30000   ! gets flow to vary from 30000 to 90000
+   flow_out(i) = (flow_out(i)/35.315)*60*60*24   ! converts ft3/sec to m3/sec
 
    ! NOTE: don't need these two flows - tried to vary these individual, but
    ! decided not to.
@@ -73,10 +77,12 @@ do  i=0,10*365
    ! ------ get flow out of reservoir for the year ------
    flow_hout(i) =  sin( i/flow_constant)
    flow_hout(i) = (flow_hout(i) + 2)*24000   ! gets flow to vary from 24000 to 72000
+   flow_hout(i) = (flow_hout(i)/35.315)*60*60*24   ! converts cfs to m3/sec
 
    ! ------ get flow out of reservoir for the year ------
    flow_eout(i) =  sin( i/flow_constant)
    flow_eout(i) = (flow_eout(i) + 2)*6000  ! gets outflow to vary from 6000 to 18000
+   flow_eout(i) = (flow_eout(i)/35.315)*60*60*24   ! converts cfs to m3/sec
 
 end  do
 
@@ -104,8 +110,8 @@ do  i=0,10*365
   ! gets net energy (positive downward) to vary from:
   ! " +0.7)*120":  -36 to 204 W/m2
   ! " +0.5)*120":  -60 to 180 W/m2
-  ! converts energy from W/m2 to Joules/m2 * day
-   energy(i) =( (energy(i) + 0.5)*120) * (24*60*60)
+  ! units are  W/m2 or Joules/m2 * sec
+   energy(i) =( (energy(i) + 0.5)*120)*60*60*24 !converts to Joules/m2 * day 
 end  do
 
 ! print *, "energy from 2000 to 2100"
@@ -119,7 +125,7 @@ density = 1000 !  density of water in kg / m3
 heat_c = 4180  !  heat capacity of water in joules/ kg * C
 
 ! initial variables
-depth_total = 30
+depth_total = 20
 depth_e = 5
 depth_h = 15
 volume_e_x = area*depth_e
@@ -131,7 +137,7 @@ temp_hypo(1) = 5 ! starting hypolimnion temperature at 5 C
 do  i=2,10*365
 
   ! calculate incoming net energy (J/m2*day) to epilimnion
-  energy_x = (energy(i)*area)/(density*heat_c*volume_e_x)
+  energy_x = energy(i)*area
 
   ! divide incoming flow into reservoir to epilimnion and hypolimnion
   flow_in_hyp_x = flow_in(i)*prcnt_flow_epil
@@ -143,13 +149,15 @@ do  i=2,10*365
   ! calculate temperature change due to diffusion
   ! NOTE: don't need to multiply by heat capacity or density of water because
   !       energy component is divided by those two
-  dif_epi_x  = ( v_t * area * (temp_hypo(i-1) - temp_epil(i-1)))/volume_e_x
-  dif_hyp_x  = ( v_t * area * (temp_epil(i-1) - temp_hypo(i-1)))/volume_h_x
+  dif_epi_x  = v_t * area * density * heat_c * (temp_hypo(i-1) - temp_epil(i-1))
+  dif_hyp_x  = v_t * area * density * heat_c * (temp_epil(i-1) - temp_hypo(i-1))
 
   ! calculate change in EPILIMNION  temperature (celsius)
-  flow_epi_x = flow_in_epi_x*flow_Tin(i)
-  flow_epi_x = (flow_epi_x - flow_out_epi_x*temp_epil(i-1))/volume_e_x
+  flow_epi_x = flow_in_epi_x * density * heat_c * flow_Tin(i)
+  flow_epi_x = flow_epi_x - (flow_out_epi_x * density * heat_c *  temp_epil(i-1))
   temp_change_ep(i) = flow_epi_x + energy_x + dif_epi_x
+  temp_change_ep(i) = temp_change_ep(i)/(volume_e_x * density * heat_c)
+  temp_change_ep(i) = temp_change_ep(i) * delta_t
 
   ! save each temperature change component
   energy_tot(i) = energy_x
@@ -161,8 +169,11 @@ do  i=2,10*365
   temp_epil(i) = temp_epil(i-1) +  temp_change_ep(i)
 
   ! calculate change in HYPOLIMNION  temperature (celsius)
-  flow_hyp_x  = (flow_in_hyp_x*flow_Tin(i)+flow_out_hyp_x*temp_hypo(i-1))/volume_h_x
-  temp_change_hyp(i) = flow_hyp_x  +  (dif_hyp_x/volume_h_x)  ! diffusion 
+  flow_hyp_x  = flow_in_hyp_x * density * heat_c * flow_Tin(i)
+  flow_hyp_x = flow_hyp_x - (flow_out_hyp_x * density * heat_c * temp_hypo(i-1))
+  temp_change_hyp(i) = flow_hyp_x  +  dif_hyp_x  !add horizontal advection and  diffusion 
+  temp_change_hyp(i) = temp_change_hyp(i)/(volume_h_x * density * heat_c)
+  temp_change_hyp(i) = temp_change_hyp(i) * delta_t
 
   ! update hypolimnion volume for next time step
   volume_h_x = volume_h_x + (flow_in_hyp_x - flow_out_hyp_x)
@@ -174,7 +185,7 @@ do  i=2,10*365
   hypox= temp_hypo(i)*(flow_out_hyp_x/outflow_x)  ! portion of temperature from hypol.
   temp_out_tot(i) = epix + hypox
 
- if (i==2 .or. i==180 .or. i==370 .or. i==550 .or. i==720 .or. i==3649) then
+ if (i==2 .or. i==180  .or. i==365 .or. i==550 .or. i==720 .or. i==3649) then
   print *, "run: ", i
 !  print *,"energy of incoming radiation " , energy(i)/(60*60*24)
 !  print *, "energy - joules/day*m2", energy(i)
@@ -189,11 +200,13 @@ do  i=2,10*365
 !   print *, "change in volume - hypolim.: ", flow_in_hyp_x - flow_out_hyp_x
 !   print *, "depth of epilimnion: ", volume_e_x/area
 !   print *, "depth of hypolimnion: ", volume_h_x/area
-!   print *, "energy temp change is : ",energy_x
-!   print *, "diffusion temp change  is: ", dif_epi_x
-!   print *, "flow epilim.  temp change is: ", flow_epi_x
+!  print *, "energy temp change is : ",energy_x/(volume_e_x * density * heat_c)
+!  print *, "diffus temp change  is: ", dif_epi_x/(volume_e_x * density *heat_c)
+!  print *, "Qin temp change: ", (flow_in_epi_x * flow_Tin(i))/volume_e_x
+!  print *, "Qout temp change: ", (flow_out_epi_x * temp_epil(i-1))/volume_e_x
+!  print *, "flow epilim. temp change: ", flow_epi_x/(volume_e_x*density* heat_c)
 !   print *, "flow hypolim.  temp change is: ", flow_hyp_x
-!   print *, "depth of epilimnion: ", volume_e_x/area 
+!  print *, "depth of epilimnion: ", volume_e_x/area 
 !   print *, "temperature change in epilimnion:  ", temp_change_ep(i)
    print *, "outflow temperature from epilimnion is: ", temp_epil(i)
 !   print *, "depth of hypolimnion: ", volume_h_x/area
@@ -211,11 +224,11 @@ end  do
 
 write(10,*)  temp_change_ep
 
- print *,"sum of 1:720 temperature change in epilim.: ", sum(temp_change_ep(1:720))
- print *,"sum of 1:720 temperature change in hypolim.: ",  sum(temp_change_hyp(1:720))
+ print *,"total simulation temperature change in epilim.: ", sum(temp_change_ep(1:3650))
+ print *,"total simulation  temperature change in hypolim.: ",  sum(temp_change_hyp(1:3650))
 
- print *, "sum of 1:720 temp change due to flow in: ", sum(T_in_tot(1:720))
- print *, "sum of 1:720 temp change due to flow out: ", sum(T_out_tot(1:720))
+! print *, "sum of 1:720 temp change due to flow in: ", sum(T_in_tot(1:720))
+! print *, "sum of 1:720 temp change due to flow out: ", sum(T_out_tot(1:720))
 ! print *, "sum of 1:720 temp change due to energy: ", sum(energy_tot(1:720))
 ! print *, "sum of 1:720 temp change due to diffusion: ", sum(diffusion_tot(1:720))
 
